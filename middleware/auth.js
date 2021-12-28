03a3d8addfbce7cd8807528cb41f28baf328fbed
n641 <noha67357@gmail.com>
2021-12-28 15:37:11 +0200

    update method

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken')
 
-const auth = (req, res, next) =>{
+const auth = (req, res, next) => {
     try {
         const token = req.header("Authorization")
-        if(!token) return res.status(400).json({msg: "Invalid Authentication"})
-
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
-            if(err) return res.status(400).json({msg: "Invalid Authentication"})
+        if (!token) return res.status(400).json({ msg: "Invalid Authentication" })
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            console.log(err);
+            if (err) return res.status(400).json({ msg: "Invalid Authentication" })
 
             req.user = user
             next()
         })
     } catch (err) {
-        return res.status(500).json({msg: err.message})
+        return res.status(500).json({ msg: err.message })
     }
 }
 
